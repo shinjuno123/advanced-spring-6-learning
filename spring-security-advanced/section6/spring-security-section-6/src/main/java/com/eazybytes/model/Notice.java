c6cb063d52bb31508a81a84df0b6de75c0381999
Junho Shin <shinjuno123@naver.com>
2023-04-23 18:19:43 -0700

    Understand & handle CORS & CSRF issues inside Web app

---
+++ spring-security-advanced/section6/spring-security-section-6/src/main/java/com/eazybytes/model/Notice.java
@@ -0,0 +1,39 @@
+package com.eazybytes.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.sql.Date;
+
+
+@Entity
+@Table(name = "notice_details")
+@Getter
+@Setter
+public class Notice {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name = "notice_id")
+    private int noticeId;
+
+    @Column(name = "notice_summary")
+    private String noticeSummary;
+
+    @Column(name = "notice_details")
+    private String noticeDetails;
+
+    @Column(name = "notic_beg_dt")
+    private Date noticBegDt;
+
+    @Column(name = "notic_end_dt")
+    private Date noticEndDt;
+
+    @Column(name = "create_dt")
+    private Date createDt;
+
+    @Column(name = "update_dt")
+    private Date updateDt;
+}
