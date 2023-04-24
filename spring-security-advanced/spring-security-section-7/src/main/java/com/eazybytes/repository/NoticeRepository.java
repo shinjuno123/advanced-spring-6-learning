a377b42c85be0600a264120feef513b404cb85e4
Junho Shin <shinjuno123@naver.com>
2023-04-23 21:03:46 -0700

    Implement Authorization using Authorities, Roles

---
+++ spring-security-advanced/spring-security-section-7/src/main/java/com/eazybytes/repository/NoticeRepository.java
@@ -0,0 +1,16 @@
+package com.eazybytes.repository;
+
+
+import com.eazybytes.model.Notice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoticeRepository extends CrudRepository<Notice, Long> {
+    @Query(value = "from Notice n where CURDATE() BETWEEN n.noticBegDt AND n.noticEndDt")
+    List<Notice> findAllActiveNotices();
+
+}
