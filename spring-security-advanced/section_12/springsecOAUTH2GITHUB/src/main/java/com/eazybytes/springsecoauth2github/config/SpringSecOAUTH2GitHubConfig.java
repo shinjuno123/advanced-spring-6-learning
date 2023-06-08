de99007864de0af70692c66a736b9483a8f1d036
Junho Shin <shinjuno123@naver.com>
2023-06-08 08:53:25 -0700

    Learning OAuth implementation

---
+++ spring-security-advanced/section_12/springsecOAUTH2GITHUB/src/main/java/com/eazybytes/springsecoauth2github/config/SpringSecOAUTH2GitHubConfig.java
@@ -0,0 +1,34 @@
+package com.eazybytes.springsecoauth2github.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecOAUTH2GitHubConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests)->requests.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRepository() {
+        ClientRegistration clientReg = clientRegistration();
+        return new InMemoryClientRegistrationRepository(clientReg);
+    }
+
+    private ClientRegistration clientRegistration() {
+		return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("7fb061f8ae16ceb0e6a0")
+	           .clientSecret("6540f6d20523efa9248f418261ca5f2f0d466d4a").build();
+	 }
+
+}
